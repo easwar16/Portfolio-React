cc73a6fa3249f6d0c89523b56009a0e9123ae842
easwar <easwarharikaran1610@gmail.com>
2023-09-02 10:57:27 +0530

    backend setup done

--- backend/sanity.config.ts
+++ backend/sanity.config.ts
@@ -1,11 +1,12 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
+import testimonials from './schemas/testimonials'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'easwar\'s_portfolio',
+  title: "easwar's_portfolio",
 
   projectId: 'dz009swe',
   dataset: 'production',
