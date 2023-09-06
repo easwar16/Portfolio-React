5d52d669015a027614b93cb295ff6908a9c180ed
easwar <easwarharikaran1610@gmail.com>
2023-09-06 13:41:14 +0530

    css basic

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import "./Navbar.scss";
+import { Images } from "../../constants";
 const Navbar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <div>
+        <img src={Images.logo} alt="" />
+      </div>
+      <ul>
+        {["home", "about", "contact", "work", "skills", "contact"].map(
+          (item) => (
+            <li key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
+      </ul>
+      {/* <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul> */}
+    </nav>
+  );
 };
 
 export default Navbar;
